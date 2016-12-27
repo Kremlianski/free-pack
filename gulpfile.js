d6ede17726c6d79752072d143d1ae9dfd1b08d9b
Alexandre Kremlianski <kremlianski@gmail.com>
2016-12-27 20:09:35 +0300

    updates 1
    add package.json
    update gulpfile.js
    complile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp'),
 
 
-  concat = require('gulp-concat'),
   coffee = require('gulp-coffee'),
   uglify = require('gulp-uglify'),
   rename = require('gulp-rename'),
@@ -10,63 +9,32 @@ var gulp = require('gulp'),
 
 
 
-
-var path = 'src/coffee/';
-var files = [
-  path + 'mmg.coffee',
-  path + 'Utility/style-detector.coffee',
-  path + 'Data/models.coffee',
-  path + 'Filter/canvas-filters.coffee',
-  path + 'Utility/queue.coffee',
-  path + 'Filter/canvas-filter.coffee',
-  path + 'View/template.coffee',
-  path + 'Utility/queue-singleton.coffee',
-  path + 'Utility/queue-simple.coffee',
-  path + 'Drawing/drawing-svg.coffee',
-  path + 'Drawing/drawing.coffee',
-  path + 'View/rows.coffee',
-  path + 'View/row.coffee',
-  path + 'View/image-view.coffee',
-  path + 'Utility/parser.coffee',
-  path + 'Utility/image-loader.coffee',
-  path + 'View/view.coffee',
-  path + 'Data/data-core.coffee',
-  path + 'Grid/defaults-core.coffee',
-  path + 'Lightbox/swipe.coffee',
-  path + 'Lightbox/lightbox-swipe.coffee',
-  path + 'Lightbox/lightbox-old.coffee',
-  path + 'AJAX/ajax.coffee',
-  path + 'Utility/natural-size.coffee',
-  path + 'Data/model-builder.coffee',
-  path + 'Lightbox/External.coffee',
-  path + 'Grid/grid.coffee',
-  path + 'gallery.coffee',
-  path + 'Templates/simple.coffee'
-];
-
-
 gulp.task('js', function () {
-  return gulp.src(files)
-    .pipe(concat('bundle.coffee'))
-    .pipe(gulp.dest('src/coffee/'))
+  return gulp.src('./templates/coffee/*.coffee')
     .pipe(coffee({
       bare: false
     }))
-    .pipe(rename('moon-mega-grid.js'))
-    .pipe(gulp.dest('src/js/'))
+    .pipe(gulp.dest('./templates/js/'))
+    .pipe(rename(function(path){
+      path.basename += '.min';
+      return path;
+   }))
     .pipe(uglify())
-    .pipe(rename('moon-mega-grid.min.js'))
-    .pipe(gulp.dest('dest/js/'));
+    .pipe(gulp.dest('templates/js/'));
 });
 
 
 
 gulp.task('css', function () {
-  return gulp.src(['src/less/mmg.less', 'src/less/mmg-class-colors.less'])
+  return gulp.src('./templates/less/*.less')
     .pipe(less())
-    .pipe(gulp.dest('src/css/'))
+    .pipe(gulp.dest('templates/css/'))
+    .pipe(rename(function(path){
+      path.basename += '.min';
+      return path;
+   }))
     .pipe(csso())
-    .pipe(gulp.dest('dest/css/'));
+    .pipe(gulp.dest('templates/css/'));
 });
 
 gulp.task('default', ['js', 'css']);
---
+++ templates/js/classica.js
@@ -0,0 +1,151 @@
+
+/*
+ *
+ * Classica Template
+ *
+ */
+
+(function() {
+  var MMG;
+
+  MMG = window.MMG;
+
+  MMG.Templates.Classica = {};
+
+
+  /*
+  General Template
+   */
+
+  MMG.Templates.Classica.template = "<div class='<%=meta.NS %>-img <%= data.classList %>' data-image-id='<%= imageId %>'> <a class='<%=meta.NS %>-link' href='<%= data.pageHref %>' target='_blank'> </a> <div class='<%=meta.NS %>-icon-wrapper <%=meta.NS %>-fs'> <div class='<%=meta.NS %>-icon-container <%=meta.NS %>-fs'> <img class='<%=meta.NS %>-icon <%=meta.NS %>-fs' src='<%= data.src %>'> </div> </div> <div class='<%=meta.NS %>-caption-wrapper'> <div class='<%=meta.NS %>-f-caption'> <% if(data.title) { %> <div class='<%=meta.NS %>-title'> <%= data.title %> </div> <% } %> <% if(data.ownerName) { %> <div class='<%=meta.NS %>-descr'> by <%= data.ownerName %> </div> <% } %> </div> </div> <div class='<%=meta.NS %>-shutter'></div> <img data-src='<%= data.ownerIcon %>' src='' style='width:1px;height:1px;z-index:0;visibility:hidden;' class='swipe-owner-icon-hidden'> </div>";
+
+
+  /*
+  General Callback
+   */
+
+  MMG.Templates.Classica.callback = function() {
+    var NS, NSclass, _removeHover, events, isMobile, loadIcon, root;
+    NS = this.model.meta.NS;
+    NSclass = this.model.meta.NSclass;
+    root = this.model.meta.root;
+    isMobile = this.model.meta.isMobile;
+    root.addClass(NS + '-classica');
+    loadIcon = function($item) {
+      var $icon;
+      $icon = $item.find('.swipe-owner-icon-hidden');
+      $icon.attr('src', $icon.attr('data-src'));
+    };
+    $(root).on('click', '.mmg-img', function(e) {
+      var $this;
+      $this = $(this);
+      loadIcon($this);
+      loadIcon($this.next());
+      loadIcon($this.prev());
+    });
+
+    /**
+     *
+     * Removes 'hover' classes
+     *
+     */
+    _removeHover = function() {
+      $(NSclass + 'hover, .hover', root.get(0)).each(function() {
+        $(this).removeClass('hover').removeClass(NS + '-hover');
+      });
+    };
+    if (isMobile) {
+
+      /*
+      this block is used to simulate effects
+      on mobile devices
+       */
+      events = 'touchstart';
+      if (/IEMobile/i.test(navigator.userAgent)) {
+        events = 'MSPointerDown mouseenter';
+      }
+      $('body').on(events, function(event) {
+        var image, target;
+        target = $(event.target);
+        if ($(event.target).hasClass(NS + '-shutter')) {
+          image = target.parent();
+          if (!image.hasClass(NS + '-hover')) {
+            _removeHover();
+            image.addClass(NS + '-hover');
+            setTimeout((function() {
+              image.addClass('hover');
+              image = null;
+            }), 500);
+          }
+        } else {
+          if (!$(event.target).hasClass(NS + '-link')) {
+            _removeHover();
+          }
+        }
+      });
+    } else {
+
+      /*
+      not mobile devices
+       */
+      root.on('mouseenter', NSclass + 'img ' + NSclass + 'shutter', function() {
+        var t;
+        t = this;
+        $(t).parent().addClass('hover').addClass(NS + '-hover');
+      });
+      root.on('mouseleave', NSclass + 'img', function() {
+        $(this).removeClass('hover').removeClass(NS + '-hover');
+      });
+    }
+  };
+
+  MMG.Templates.Classica.ie9 = {};
+
+
+  /*
+  Template for IE8
+   */
+
+  MMG.Templates.Classica.ie9.template = "<div class='<%=meta.NS %>-img <%= data.classList %>' data-image-id='<%= imageId %>'> <a class='<%=meta.NS %>-link' href='<%= data.pageHref %>' target='_blank'> <div class='<%=meta.NS %>-icon-wrapper <%=meta.NS %>-fs'> <div class='<%=meta.NS %>-icon-container <%=meta.NS %>-fs'> <img class='<%=meta.NS %>-icon <%=meta.NS %>-fs' src='<%= data.src %>'> </div> </div> <div class='<%=meta.NS %>-caption-wrapper'> <div class='<%=meta.NS %>-f-caption'> <% if(data.title) { %> <div class='<%=meta.NS %>-title'> <%= data.title %> </div> <% } %> <% if(data.ownerName) { %> <div class='<%=meta.NS %>-descr'> by <%= data.ownerName %> </div> <% } %> </div> </div> </a> <img data-src='<%= data.ownerIcon %>' src='' style='width:1px;height:1px;z-index:0;visibility:hidden;' class='swipe-owner-icon-hidden'> </div>";
+
+
+  /*
+  Callback for IE9, IE8
+   */
+
+  MMG.Templates.Classica.ie9.callback = function() {
+    var NS, NSclass, loadIcon, root;
+    NS = this.model.meta.NS;
+    NSclass = this.model.meta.NSclass;
+    root = this.model.meta.root;
+    loadIcon = function($item) {
+      var $icon;
+      $icon = $item.find('.swipe-owner-icon-hidden');
+      $icon.attr('src', $icon.attr('data-src'));
+    };
+    $(root).on('click', '.mmg-img', function(e) {
+      var $this;
+      $this = $(this);
+      loadIcon($this);
+      loadIcon($this.next());
+      loadIcon($this.prev());
+    });
+    root.on('mouseenter', NSclass + 'img', function() {
+      $(NSclass + 'caption-wrapper', this).fadeIn();
+    });
+    root.on('mouseleave', NSclass + 'img', function() {
+      $(NSclass + 'caption-wrapper', this).fadeOut();
+    });
+  };
+
+
+  /*
+  Defaults options
+   */
+
+  MMG.Templates.Classica.defaults = {
+    templateName: 'Classica',
+    gridClass: 'classica'
+  };
+
+}).call(this);
